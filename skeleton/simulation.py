af6e62feeb6a058ade2eb9def0bc11efde85c13f
Yannick Mueller <muelleya@student.ethz.ch>
2020-11-07 20:15:19 +0100

    Score Function

--- skeleton/simulation.py
+++ skeleton/simulation.py
@@ -48,41 +48,66 @@ class _BaseSimulation:
             # Update the stats that we use for scoring
             self.update_stats()
 
-class _Stage1Scorer(_BaseSimulation):
-    N_BUSES_WEIGHT=1000
-    NOT_ARRIVED_WEIGHT=1
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.not_arrived_persons = 0
-        self.buses = set()
-    
-    def update_stats(self):
-        self.buses|={id for id in traci.vehicle.getIDList() if id.startswith("bus")}
-    
-    def get_score(self):
-        not_arrived = sum([1 for id in traci.person.getIDList() if "Arrived" not in traci.person.getStage(id).description])
-        return len(self.buses)*self.N_BUSES_WEIGHT + not_arrived*self.NOT_ARRIVED_WEIGHT
-
-class _Stage2Scorer(_BaseSimulation):
+class _StageScorer(_BaseSimulation):
     N_BUSES_WEIGHT=1000
     WAIT_TIME_WEIGHT=1
+    NOT_ARRIVED_WEIGHT=1
+    DRIVEN_DISTANCE_WEIGHT=1
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.total_waiting_time = 0
         self.buses = set()
+        self.distance_dict = dict()
     
     def update_stats(self):
         person_ids = traci.person.getIDList()
         self.total_waiting_time += sum([1 for id in person_ids if traci.person.getStage(id).type == 1])
-        self.total_waiting_time += sum([1 for id in person_ids if traci.person.getStage(id).type == 1])
-        self.buses|={id for id in traci.vehicle.getIDList() if id.startswith("bus")}
-    
-    def get_score(self):
-        return len(self.buses)*self.N_BUSES_WEIGHT + self.total_waiting_time*self.WAIT_TIME_WEIGHT
+        self.buses |= {id for id in traci.vehicle.getIDList() if id.startswith("bus")}
 
-class ExampleSimulation(_Stage1Scorer):
+        for id in list(self.buses):
+            new_dist = 0
+            try: 
+                new_dist = traci.vehicle.getDistance(id)
+            except:
+                pass
+
+            vehicle_id = None
+            try:
+                vehicle_id = traci.vehicle.getTypeID(id)
+            except:
+                pass
+
+            if id in self.distance_dict:
+                self.distance_dict[id] = (max(self.distance_dict[id][0], new_dist), vehicle_id if self.distance_dict[id][1]==None else self.distance_dict[id][1])
+            else:
+                
+                self.distance_dict[id] = (new_dist, vehicle_id)
+
+
+    def get_score(self):
+        not_arrived = sum([1 for id in traci.person.getIDList() if "Arrived" not in traci.person.getStage(id).description])
+        driven_distance_all = 0
+        driven_distance_l = 0
+        buses_l = 0
+
+        for dist_vehicle in self.distance_dict.values():
+            print(dist_vehicle[0], dist_vehicle[1])
+            if dist_vehicle[1] =="BUS_L":
+                driven_distance_l += dist_vehicle[0]
+                buses_l += 1
+            driven_distance_all += dist_vehicle[0]
+
+        stage1 = buses_l*self.N_BUSES_WEIGHT + not_arrived*self.NOT_ARRIVED_WEIGHT
+
+        stage2 =  stage1+ self.total_waiting_time*self.WAIT_TIME_WEIGHT + driven_distance_l * self.DRIVEN_DISTANCE_WEIGHT
+
+        stage3 = len(self.buses)*self.N_BUSES_WEIGHT + not_arrived*self.NOT_ARRIVED_WEIGHT + self.total_waiting_time*self.WAIT_TIME_WEIGHT + driven_distance_all * self.DRIVEN_DISTANCE_WEIGHT
+        
+        return (stage1, stage2, stage3)
+
+class ExampleSimulation(_StageScorer):
     """ The example simulation provided by ELCA
     """
     def setup(self):
